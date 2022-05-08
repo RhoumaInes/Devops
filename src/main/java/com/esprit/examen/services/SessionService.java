2b1a641a594f29e42e5e25a20f8d8832dabbf4c9
belheni marwa <belhenimarwa@gmail.com>
2022-05-08 09:49:27 -0400

    first commit

---
+++ src/main/java/com/esprit/examen/services/SessionService.java
@@ -0,0 +1,49 @@
+package com.esprit.examen.services;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.esprit.examen.entities.Contrat;
+import com.esprit.examen.entities.Formateur;
+import com.esprit.examen.entities.Session;
+import com.esprit.examen.repositories.FormateurRepository;
+import com.esprit.examen.repositories.SessionRepository;
+
+@Service
+public class SessionService implements ISessionService{
+
+	@Autowired
+	SessionRepository sessionRepository;
+	@Autowired
+	FormateurRepository frm;
+	@Override
+	public Long addSession(Session session) {
+		sessionRepository.save(session);
+		return session.getId();
+	}
+
+	@Override
+	public Long modifierSession(Session session) {
+		sessionRepository.save(session);
+		return session.getId();
+	}
+
+	@Override
+	public void supprimerSession(Long sessionId) {
+		sessionRepository.deleteById(sessionId);
+	}
+
+	@Override
+	public void affecterFormateurASession(Long formateurId, Long sessionId) {
+	   Session s = sessionRepository.findById(sessionId).get();
+	   Formateur f=frm.findById(formateurId).get();
+	   Set<Session> list = f.getSessions();
+	   list.add(s);
+	   f.setSessions(list);
+	   frm.save(f);
+	
+	}
+
+}
---
+++ src/test/java/com/esprit/examen/SessionServiceTest.java
@@ -0,0 +1,92 @@
+package com.esprit.examen;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.text.ParseException;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.esprit.examen.entities.Formateur;
+import com.esprit.examen.entities.Session;
+import com.esprit.examen.repositories.FormateurRepository;
+import com.esprit.examen.repositories.SessionRepository;
+import com.esprit.examen.services.FormateurService;
+import com.esprit.examen.services.SessionService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@Slf4j
+public class SessionServiceTest {
+
+	@Autowired
+	SessionService service;
+	@Autowired
+	SessionRepository rep;
+	@Autowired
+	FormateurService fs;
+	@Autowired
+	FormateurRepository repF;
+	
+	/*@Test
+	public void testAddSession() {
+		Long savedSeesion = service.addSession(new Session ("The DevOps Handbook", 50L));
+		Session s = rep.findById(savedSeesion).get();
+		log.info(s.getDescription());
+		assertThat(savedSeesion).isGreaterThan(0);
+		//slog.info("sessione ajoutee!!!"+savedSeesion);
+		try {
+			rep.findById(savedSeesion);
+			log.info("sesion ajoutee");
+		} catch (Exception e) {
+			log.error(savedSeesion+ "non ajoutee :D");
+		}
+	}*/
+	
+	@Test
+	public void testSuppSession() {
+		Long savedSession = service.addSession(new Session ("The DevOps", 90L));
+		Session s = rep.findById(savedSession).get();
+	    service.supprimerSession(savedSession);
+	    assertNull(rep.findById(savedSession).get());
+	    log.info("session with id"+savedSession+"has been deleted");
+	}
+	
+	/*@Test
+	public void testmodifSession() {
+		Long savedSession = service.addSession(new Session ("The DevOps", 90L));
+		Session s = rep.findById(savedSession).get();
+		s.setDescription("soa");
+		s.setDuree(23L);
+		Long updatedSession= service.modifierSession(s);
+		s = rep.findById(updatedSession).get();
+		if ((s.getDescription().equals("soa")) & (s.getDuree().equals(23L)) ){
+			log.info("it was modified"+updatedSession);
+		}
+		    log.error("it was not modified");
+		}
+		
+		
+    @Test
+    public void testAffecterFormateurASession() {
+    	Long savedSession = service.addSession(new Session ("The DevOps", 90L));
+    	Long savedFormateur = fs.addFormateur(new Formateur("marwa", "belheni"));
+    	service.affecterFormateurASession(savedFormateur, savedSession);
+    	Session s=rep.findById(savedSession).get();
+    	Formateur f = repF.findById(savedFormateur).get();
+    	assertTrue(s.getFormateur().equals(f));
+    	log.info("affectation efectuee avec succes du formateur "+savedFormateur+"a la session"+savedSession);
+    	
+    }*/
+		
+		
+		
+	
+}
